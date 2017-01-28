a10e5d5812517dad6ddb32c63be46ee65e4a1e84
cendhikaimantoro <cendhika.i@gmail.com>
2017-01-28 20:11:46 +0700

    update menambah fungsi untuk memeriksa putus-tidaknya lingkaran

--- lingkaran.c
+++ lingkaran.c
@@ -58,6 +58,78 @@ void gambarLingkaran(lingkaran* L, matrix* M, char c) {
     }
 }
 
+int isLingkaranPutus(lingkaran* L, matrix* M, char c) {
+    int ret = 0;
+    if (sizeof(L) != 0){
+        if ((sizeof(M -> xsize)) != 0) {
+            if (sizeof(M -> ysize != 0)) {
+                    int x = L->x;
+                    int y = L->y + L->r;
+                    int err = 0;
+                    while (x - L->x <= y - L->y && ret == 0) {
+                        int dx = x - L->x;
+                        int dy = y - L->y;
+                        if (L->x + dx >= 0 && L->x + dx < M->xsize && L->y + dy >= 0 && L->y + dy < M->ysize) {
+                            if (M -> M[L->x + dx][L->y + dy] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x - dx >= 0 && L->x - dx < M->xsize && L->y + dy >= 0 && L->y + dy < M->ysize) {
+                            if (M -> M[L->x - dx][L->y + dy] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x + dx >= 0 && L->x + dx < M->xsize && L->y - dy >= 0 && L->y - dy < M->ysize) {
+                            if (M -> M[L->x + dx][L->y - dy] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x - dx >= 0 && L->x - dx < M->xsize && L->y - dy >= 0 && L->y - dy < M->ysize) {
+                            if (M -> M[L->x - dx][L->y - dy] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x + dy >= 0 && L->x + dy < M->xsize && L->y + dx >= 0 && L->y + dx < M->ysize) {
+                            if (M -> M[L->x + dy][L->y + dx] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x - dy >= 0 && L->x - dy < M->xsize && L->y + dx >= 0 && L->y + dx < M->ysize) {
+                            if (M -> M[L->x - dy][L->y + dx] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x + dy >= 0 && L->x + dy < M->xsize && L->y - dx >= 0 && L->y - dx < M->ysize) {
+                            if (M -> M[L->x + dy][L->y - dx] != c) {
+                                ret = 1;
+                            };
+                        }
+                        if (L->x - dy >= 0 && L->x - dy < M->xsize && L->y - dx >= 0 && L->y - dx < M->ysize) {
+                            if (M -> M[L->x - dy][L->y - dx] != c) {
+                                ret = 1;
+                            };
+                        }
+
+                        if (err <= 0)
+                        {
+                            dx += 1;
+                            err += 2*dx + 1;
+                        }
+                        if (err > 0)
+                        {
+                            dy -= 1;
+                            err -= 2*dy + 1;
+                        }
+
+                        x = L->x + dx;
+                        y = L->y + dy;
+                }
+            }
+        }
+    }
+    return ret;
+}
+
 int main() {
 
     matrix M;
@@ -75,28 +147,57 @@ int main() {
         }
     }
 
-    lingkaran L;
+    lingkaran L1, L2, L3, L4;
 
-    L.x = 10;
-    L.y = 15;
-    L.r = 20;
-    gambarLingkaran(&L, &M, '1');
+    L1.x = 10;
+    L1.y = 15;
+    L1.r = 20;
+    gambarLingkaran(&L1, &M, '1');
 
-    L.x = 30;
-    L.y = 25;
-    L.r = 15;
-    gambarLingkaran(&L, &M, '2');
+    L2.x = 30;
+    L2.y = 25;
+    L2.r = 15;
+    gambarLingkaran(&L2, &M, '2');
 
-    L.x = 40;
-    L.y = 15;
-    L.r = 18;
-    gambarLingkaran(&L, &M, '3');
+    L3.x = 40;
+    L3.y = 15;
+    L3.r = 18;
+    gambarLingkaran(&L3, &M, '3');
+
+    L4.x = 5;
+    L4.y = 5;
+    L4.r = 5;
+    gambarLingkaran(&L4, &M, '4');
 
     for (i = 0; i < 50; i++) {
         for (j = 0; j < 50; j++) {
             printf("%c",M.M[j][i]);
         }
         printf("\n");
+    }
+
+    if (isLingkaranPutus(&L1, &M, '1') == 1) {
+        printf("Lingkaran 1 terputus\n");
+    } else {
+        printf("Lingkaran 1 tidak terputus\n");
+    }
+
+    if (isLingkaranPutus(&L2, &M, '2') == 1) {
+        printf("Lingkaran 2 terputus\n");
+    } else {
+        printf("Lingkaran 2 tidak terputus\n");
+    }
+
+    if (isLingkaranPutus(&L3, &M, '3') == 1) {
+        printf("Lingkaran 3 terputus\n");
+    } else {
+        printf("Lingkaran 3 tidak terputus\n");
+    }
+
+    if (isLingkaranPutus(&L4, &M, '4') == 1) {
+        printf("Lingkaran 4 terputus\n");
+    } else {
+        printf("Lingkaran 4 tidak terputus\n");
     }
 
     return 0;
--- lingkaran.h
+++ lingkaran.h
@@ -4,11 +4,16 @@
 
 typedef struct
 {
-	int x,
-	int y,
-	int r
+	int x, y, r;
 } lingkaran;
 
+//Menggambar lingkaran pada matriks
 void gambarLingkaran(lingkaran* L, matrix* M, char c);
+
+//Memeriksa apakah semua bagian lingkaran pada L pada matriks M berisi karakter c
+//Mengembalikan 0 jika semuanya berisi karakter c
+//Mengembalikan 1 jika ada bagian yang tertimpa karakter lain
+int isLingkaranPutus(lingkaran* L, matrix* M, char c);
+
 
 #endif
